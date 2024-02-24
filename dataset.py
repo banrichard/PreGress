a7ebbd2990fa7655d17540c5114989835b5cc197
banlujie <richard190400@outlook.com>
2024-02-24 17:23:32 +0800

    project init

---
+++ dataset.py
@@ -0,0 +1,15 @@
+from torch_geometric.datasets import ZINC,QM9
+import os
+
+
+def dataset_load(name="ZINC", type="train"):
+    if name == "ZINC":
+        data = ZINC(root=os.path.join("/mnt/data/lujie/metacounting_dataset", name), split=type)
+    else:
+        data = QM9(root=os.path.join("/mnt/data/lujie/metacounting_dataset", name))
+    return data
+
+
+if __name__ == "__main__":
+    data = dataset_load(name="QM9")
+    print(data[0])
